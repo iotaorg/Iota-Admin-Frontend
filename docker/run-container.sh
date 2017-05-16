f4d67d7171d55b3026e6645aa5bbfee9d5e95b39
Renato Cron <renato.cron@gmail.com>
2017-05-16 14:31:20 -0300

    Add regras pra Primeira Infancia

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -3,6 +3,10 @@
 export GIT_DIR=$(git rev-parse --show-toplevel)
 export MYIP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
 
+if [ -z "$MYIP" ]; then
+    MYIP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f7)
+fi
+
 echo "Make sure API is listening on $MYIP:5000 ";
 (sleep 1; export X=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' iota-frontend 2>&1`; echo "http://$X/frontend" ) &
 
